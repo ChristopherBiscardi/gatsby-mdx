8a596c5b7fbb7533117e5a5a6b8790f26888b1e3
Christopher Biscardi <biscarch@sketcht.com>
2018-07-22 23:00:24 -0700

    init docs site

---
+++ examples/docs/gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-mdx docs`
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-offline`
+  ]
+};
