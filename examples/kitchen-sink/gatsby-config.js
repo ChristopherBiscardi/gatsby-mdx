11d7635ed9b0ca6bad4aaf01c2fba0fa2c20cbd5
Christopher Biscardi <biscarch@sketcht.com>
2018-07-23 01:48:34 -0700

    mdx pages

--- examples/kitchen-sink/gatsby-config.js
+++ examples/kitchen-sink/gatsby-config.js
@@ -1,16 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby with Emotion`,
+    title: `Gatsby with Emotion`
   },
   plugins: [
     `gatsby-plugin-emotion`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-mdx`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `UA-93349937-2`,
-      },
+        trackingId: `UA-93349937-2`
+      }
     },
-    `gatsby-plugin-offline`,
-  ],
-}
+    `gatsby-plugin-offline`
+  ]
+};
