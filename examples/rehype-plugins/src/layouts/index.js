b1ec4c8375276b9da847cc8b05f18d41d1dceb2e
Christopher Biscardi <biscarch@sketcht.com>
2018-08-20 18:30:06 -0700

    create ui-extensions workspace and fix lint

--- examples/rehype-plugins/src/layouts/index.js
+++ examples/rehype-plugins/src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import { graphql } from 'gatsby'
 
 import Header from '../components/header'
 import './index.css'
