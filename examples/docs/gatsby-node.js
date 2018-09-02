744f60102660ff96decafb5e6dbf29a7ce483c34
Gregg B <514026+greggb@users.noreply.github.com>
2018-09-02 01:49:49 -0400

    Add an alias for docs components (#107)
    Fixes #103

--- examples/docs/gatsby-node.js
+++ examples/docs/gatsby-node.js
@@ -53,7 +53,8 @@ exports.createPages = ({ graphql, actions }) => {
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
-      modules: [path.resolve(__dirname, "src"), "node_modules"]
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
+      alias: { $components: path.resolve(__dirname, "src/components") }
     }
   });
 };
