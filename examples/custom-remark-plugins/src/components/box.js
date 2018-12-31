103ab3afa10f0cd20fd853424abae0130e737c48
ChristopherBiscardi <chris@christopherbiscardi.com>
2018-12-30 18:05:42 -0800

    upgrade custom-remark-plugins example

---
+++ examples/custom-remark-plugins/src/components/box.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Box = props => <div {...props} />;
+
+export default Box;
--- examples/custom-remark-plugins/src/pages/index.js
+++ examples/custom-remark-plugins/src/pages/index.js
@@ -1,46 +1,22 @@
 import React, { Fragment } from "react";
 import Helmet from "react-helmet";
-import { injectGlobal } from "emotion";
-import styled from "react-emotion";
 import { graphql, Link } from "gatsby";
+import { css } from "@emotion/core";
 
-injectGlobal`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`;
-
-injectGlobal`
-  html, body {
-    font-family: -apple-system,
-      BlinkMacSystemFont,
-      "Segoe UI",
-      "Roboto",
-      "Roboto Light",
-      "Oxygen",
-      "Ubuntu",
-      "Cantarell",
-      "Fira Sans",
-      "Droid Sans",
-      "Helvetica Neue",
-      sans-serif,
-      "Apple Color Emoji",
-      "Segoe UI Emoji",
-      "Segoe UI Symbol";
-  }
-`;
-
-const Wrapper = styled.section`
-  align-items: center;
-  background: #282a36;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  justify-content: center;
-  width: 100vw;
-`;
+const Wrapper = props => (
+  <section
+    css={css`
+      align-items: center;
+      background: #282a36;
+      display: flex;
+      flex-direction: column;
+      height: 100vh;
+      justify-content: center;
+      width: 100vw;
+    `}
+    {...props}
+  />
+);
 
 const IndexPage = ({ data }) => (
   <Fragment>
@@ -50,55 +26,22 @@ const IndexPage = ({ data }) => (
       <meta name="referrer" content="origin" />
     </Helmet>
     <Wrapper>
-      <h1
-        css={`
-          font-size: 1.5em;
-          color: #ff79c6;
-          margin-bottom: 0.5em;
-        `}
-      >
-        Hello World, this is my first component styled with
-        {` `}
-        <a
-          css={`
-            color: #8be9fd;
-          `}
-          href="https://emotion.sh/"
-        >
-          emotion
-        </a>
-        !
-      </h1>
-      <p
-        css={`
-          color: #bd93f9;
-        `}
-      >
-        <a
-          href="https://www.gatsbyjs.org/packages/gatsby-plugin-emotion/"
-          css={`
-            color: inherit;
-          `}
-        >
-          gatsby-plugin-emotion docs
-        </a>
-      </p>
       <h2
-        css={`
+        css={css`
           margin-top: 1.5em;
         `}
       >
         Example Pages
       </h2>
       <ul
-        css={`
+        css={css`
           list-style-type: none;
         `}
       >
         {data.allSitePage.edges.map(({ node }) => (
           <li key={node.id}>
             <Link
-              css={`
+              css={css`
                 color: #8be9fd;
               `}
               to={node.path}
