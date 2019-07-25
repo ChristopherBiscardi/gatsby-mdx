b9d2c13d9eab19b207831514e246b6b987aaa16f
Christopher Biscardi <chris@christopherbiscardi.com>
2019-07-24 20:41:27 -0700

    stuff

---
+++ examples/docs/public/component---src-pages-index-js-e2c82fdaa5b3a4c37d92.js
@@ -0,0 +1,35 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [1],
+  {
+    187: function(e, t, n) {
+      "use strict";
+      n.r(t);
+      var a = n(1),
+        s = n.n(a);
+      t.default = function() {
+        return s.a.createElement(
+          "div",
+          null,
+          s.a.createElement(
+            "h1",
+            null,
+            "gatsby-mdx is deprecated, use ",
+            s.a.createElement(
+              "a",
+              { href: "https://www.gatsbyjs.org/docs/mdx/" },
+              "gatsby-plugin-mdx"
+            ),
+            " ",
+            "instead"
+          ),
+          s.a.createElement(
+            "p",
+            null,
+            "gatsby-plugin-mdx is the continuation of gatsby-mdx post-1.0."
+          )
+        );
+      };
+    }
+  }
+]);
+//# sourceMappingURL=component---src-pages-index-js-e2c82fdaa5b3a4c37d92.js.map
