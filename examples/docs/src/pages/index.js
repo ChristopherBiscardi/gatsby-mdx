199e2f7a4f1bc520ccbc383232b86b44ef3b9787
Christopher Biscardi <chris@christopherbiscardi.com>
2019-07-24 20:22:41 -0700

    docs

---
+++ examples/docs/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1>
+        gatsby-mdx is deprecated, use&nbsp;
+        <a href="https://www.gatsbyjs.org/docs/mdx/">gatsby-plugin-mdx</a>{" "}
+        instead
+      </h1>
+      <p>gatsby-plugin-mdx is the continuation of gatsby-mdx post-1.0.</p>
+    </div>
+  );
+};
